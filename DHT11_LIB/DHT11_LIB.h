eb8a5af56e1a7cd48153f612f4b9b3d8f66823eb
bafeigum <bafeigum@gmail.com>
2013-06-12 15:26:55 -0600

    Update DHT11_LIB.h

--- DHT11_LIB/DHT11_LIB.h
+++ DHT11_LIB/DHT11_LIB.h
@@ -1,11 +1,27 @@
 /*
- * DHT11_LIB
- * DHT11_LIB.h
- *
+ * 
  * Engineer: Bryce Feigum (bafeigum at gmail)
  * Date: December 22, 2012
- * Version: 1.0
  *
+ * Version: 1.0 - Initial Release
+ * Version: 1.1 - Added GNUv2 License
+ *
+ * Copyright (C) 2013  Bryce Feigum
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Project:  DHT11_LIB
+ * File:	DHT11_LIB.h
  */
 
 #ifndef DHT11_LIB_H_
